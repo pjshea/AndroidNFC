e96c1d4fc8cd8747444599d9441ef7c00f80e5c7
pjshea <pjshea@gmail.com>
2018-04-18 21:25:56 -0400

    Fixed another merge defect

--- app/src/main/java/edu/njit/cs656/nfcapp/MainActivity.java
+++ app/src/main/java/edu/njit/cs656/nfcapp/MainActivity.java
@@ -68,7 +68,7 @@ public class MainActivity extends Activity {
 
     public void sendContact(View view) {
 
-        Intent inent = new Intent(this, SendPictureActivity.class);
+        Intent inent = new Intent(this, SendContactActivity.class);
 
         // calling an activity using <intent-filter> action name
         //  Intent inent = new Intent("com.hmkcode.android.ANOTHER_ACTIVITY");
@@ -78,7 +78,7 @@ public class MainActivity extends Activity {
 
     public void sendMessage(View view) {
 
-        Intent inent = new Intent(this, SendPictureActivity.class);
+        Intent inent = new Intent(this, SendMessageActivity.class);
 
         // calling an activity using <intent-filter> action name
         //  Intent inent = new Intent("com.hmkcode.android.ANOTHER_ACTIVITY");
